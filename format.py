b1b2d61c707038f9723ab5f90340077e9a787f32
Tyler Veness <calcmogul@gmail.com>
2020-04-17 14:18:53 -0700

    Simplify format.py
    Black's default file exclusions for recursive searches work fine.

--- format.py
+++ format.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-import os
 import subprocess
 import sys
 
-files = [
-    os.path.join(dp, f) for dp, dn, fn in os.walk(".") for f in fn if f.endswith(".py")
-]
-
-subprocess.run([sys.executable, "-m", "black", "-q", "format.py"])
-for f in files:
-    subprocess.run([sys.executable, "-m", "black", "-q", f])
+subprocess.run([sys.executable, "-m", "black", "-q", "."])
